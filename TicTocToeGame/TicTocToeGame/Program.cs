988088c1e78cefd063fc1129b6679e3147db0352
Hitesh <hiteshspatil99@gmail.com>
2021-12-04 09:32:47 +0530

    [Hitesh] add : Tic Toc Toe Empty Space Creating

---
+++ TicTocToeGame/TicTocToeGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTocToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe Game");
+            TicTocToeGame game = new TicTocToeGame();
+            game.gameBoard();
+        }
+    }
+}
---
+++ TicTocToeGame/TicTocToeGame/TicTocToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTocToeGame
+{
+    class TicTocToeGame
+    {
+        public void gameBoard()
+        {
+            char[] board = new char[10];
+
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    board[i] = ' ';
+                }
+            }
+        }
+    }
+}
